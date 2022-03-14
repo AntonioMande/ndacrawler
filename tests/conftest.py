df816384f48b744bd9b291e4c86647bccc63ccdb
TonnyMande <mandetonny@gmail.com>
2022-03-14 11:19:13 +0300

    Temporarily remove tests

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from ndacrawler import get_drugs
-
-
-@pytest.fixture
-def drugs():
-    return get_drugs()
--- tests/test_get_drugs.py
+++
@@ -1,7 +0,0 @@
-
-
-def test_herbal_human_drugs(drugs):
-    # Data Extractors
-    herbal_human_drugs = drugs["Herbal Human"]
-    herbal_human_drugs.to_csv("Herbal Human.csv")
-    assert True
